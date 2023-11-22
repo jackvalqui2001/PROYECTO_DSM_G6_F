a326aeafa11c20e7db17139a21a6f7239df4576e
jackvalqui2001 <jack.valqui@unmsm.edu.pe>
2023-11-22 15:52:51 -0500

    Modificacion 1

--- PROYECTO_DSM_G7/app/src/main/java/com/example/proyecto_dsm_g7/model/detalle.kt
+++
@@ -1,7 +0,0 @@
-package com.example.proyecto_dsm_g7.data.model
-
-data class detalle(
-    val idMantRecibo: Int?,
-    val importeCasa : Float?,
-    val descripcion : String?,
-)
--- PROYECTO_DSM_G7/app/src/main/java/com/example/proyecto_dsm_g7/tables/detalle_Table.kt
+++
@@ -1,9 +0,0 @@
-package com.example.proyecto_dsm_g7.data.tables
-
-import org.jetbrains.exposed.sql.Table
-
-object detalle_Table: Table("detalles") {
-    val idMantRecibo = integer("id_mant_recibo")
-    val importeCasa = float("importe_casa")
-    val descripcion = varchar("descripcion",100)
-}
